cf993507bb3434adf7d92c797808e5cac9e91cce
Bernd Broermann <bernd@broermann.com>
2020-08-02 17:30:05 +0200

    Update anchor_reaction.sh

--- provision/anchor_reaction.sh
+++ provision/anchor_reaction.sh
@@ -6,7 +6,7 @@
 
 
 # iptables
-### disbale networktraffic
+### disable network traffic
 iptables --flush
 iptables -P OUTPUT DROP
 iptables -P INPUT DROP
@@ -15,3 +15,4 @@ iptables -P FORWARD DROP
 
 ###  set disks readonly
 mount |sed -n  "s/.* on \(.*\) type \(ext[a-z0-9]*\|xfs\|btrfs\|zfs\) .*/mount -o remount,ro \1 # \2/p" | sh
+
