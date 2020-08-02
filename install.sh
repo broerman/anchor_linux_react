1a10c9d0e61e76c68f49b717f1709f136bbda9b3
Bernd Broermann <bernd@broermann.com>
2020-08-02 21:34:10 +0200

    chmod

--- install.sh
+++ install.sh
@@ -11,8 +11,10 @@ curl -Ss -L https://raw.githubusercontent.com/broerman/anchor_linux_react/master
 
 # get anchor_notify.sh
 curl -Ss -L https://raw.githubusercontent.com/broerman/anchor_linux_react/master/provision/anchor_notify.sh -o /usr/local/bin/anchor_notify.sh
+chmod 755 /usr/local/bin/anchor_notify.sh
 # get anchor_reaction.sh
 curl -Ss -L https://raw.githubusercontent.com/broerman/anchor_linux_react/master/provision/anchor_reaction.sh -o /usr/local/bin/anchor_reaction.sh
+chmod 755 /usr/local/bin/anchor_reaction.sh
 # get incrontab for root
 curl -Ss -L https://raw.githubusercontent.com/broerman/anchor_linux_react/master/provision/incron_root  -o /var/spool/incron/root
 
